56948ff4a2a53cf6a9ca1e0f3eeee64ad31cca09
jatuponp <jatuponp@gmail.com>
2015-10-02 16:27:10 +0700

    Initial commit

---
+++ app/src/main/java/com/nkc/nkcpost/AppConfig.java
@@ -0,0 +1,10 @@
+package com.nkc.nkcpost;
+
+/**
+ * Created by Jumpon-pc on 30/9/2558.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "https://it.nkc.kku.ac.th/backend/api/login";
+
+}
